79f609d884970c491a0e3bcd867c84a7a9d31afe
TobenWimer <timonw896@gmail.com>
2025-08-18 18:37:41 +0200

    disable eslint during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,   // 🚀 Linter-Fehler blockieren den Build nicht mehr
+  },
 };
 
 export default nextConfig;
